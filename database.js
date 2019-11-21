d84f7e4f5bac49c566a9f3a7e694aab3561c0d51
Mark O'Reilly <mark@markoreilly.xyz>
2019-11-21 21:49:46 +0000

    added mongo db connection

---
+++ database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const dbPath = "mongodb+srv://vendy:4cfSDLPRdy%40B%23e%2A@vendy-ruokw.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(dbPath, {
+    useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('open', console.error.bind(console, 'MongoDB connected:'));
+module.exports = mongoose;

e7171924ff87de82468bd11ba67781a0f90a8a70
Mark O'Reilly <mark@markoreilly.xyz>
2019-11-23 14:38:13 +0000

    implemented async crud

--- api/v1/assistants.js
+++ api/v1/assistants.js
@@ -1,20 +1,32 @@
 const Router = require('@koa/router');
-const router = new Router();
+const router = new Router({
+  prefix: '/assistants'
+});
 const models = require("../../models")
 
 router.get('/', async (ctx) => {
-    models.Assistant.find({}, (err, docs) => {
-      if(err){
-        ctx.body = {
-          status: 'error',
-          content: err
-        }
+    try {
+      const assistants = await models.Assistant.find();
+      if (!assistants) {
+        ctx.throw(404);
       }
-      ctx.body = {
-        status: 'success',
-        content: docs
+      ctx.body = assistants;
+    } catch (err) {
+      if (err.name === 'CastError' || err.name === 'NotFoundError') {
+        ctx.throw(404);
       }
-    });
+      ctx.throw(500);
+    }
+})
+
+router.post('/', async (ctx, next) => {
+    try {
+      const body = ctx.request.body;
+      const assistant = await new models.Assistant(body).save();
+      ctx.body = assistant;
+  } catch (err) {
+      ctx.throw(422);
+  }
 })
 
 module.exports = router;

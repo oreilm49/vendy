cc46d3fa61d271835a803ffd783e1b5c24644615
Mark <markoreilly1992@gmail.com>
2019-11-23 16:38:20 +0000

    remove deprecated method

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const dbPath = "mongodb+srv://vendy:4cfSDLPRdy%40B%23e%2A@vendy-ruokw.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(dbPath, {
     useNewUrlParser: true,
+    useFindAndModify: false
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
